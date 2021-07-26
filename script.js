df018f78fb4b9d412571c94bc244d8041a8c58e6
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-07-27 01:31:51 +0200

    autoplay

--- script.js
+++ script.js
@@ -1,117 +1,110 @@
-const musicContainer = document.querySelector('.music-container')
-const playBtn = document.querySelector('#play')
-const prevBtn = document.querySelector('#prev')
-const nextBtn = document.querySelector('#next')
-const audio = document.querySelector('#audio')
-const progress = document.querySelector('.progress')
-const progressContainer = document.querySelector('.progress-container')
-const title = document.querySelector('#title')
-const cover = document.querySelector('#cover')
-
+const musicContainer = document.querySelector(".music-container");
+const playBtn = document.querySelector("#play");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+const audio = document.querySelector("#audio");
+const progress = document.querySelector(".progress");
+const progressContainer = document.querySelector(".progress-container");
+const title = document.querySelector("#title");
+const cover = document.querySelector("#cover");
 
 // Song tites
-const songs = ['Play With Fire', 'Someone You Loved', 'Hold On', 'Fight Song', 'Perfect', 'Feeling Good']
+const songs = [
+  "Play With Fire",
+  "Someone You Loved",
+  "Hold On",
+  "Fight Song",
+  "Perfect",
+  "Feeling Good",
+];
 
-// keep track of songs 
-let songIndex = 5
+// keep track of songs
+let songIndex = 5;
 
 // Initially load song info DOM
-loadSong(songs[songIndex])
+loadSong(songs[songIndex]);
 
-//Update song details 
+//Update song details
 function loadSong(song) {
-    title.innerText = song
-    audio.src = `music/${song}.mp3`
-    cover.src = `images/${song}.jpg`
-
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
 }
-
-
 
 // Play song
 function playSong() {
-    musicContainer.classList.add('play')
-    playBtn.querySelector('i.fas').classList.remove('fa-play')
-    playBtn.querySelector('i.fas').classList.add('fa-pause')
-    audio.play()
-
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+  audio.play();
 }
 
 // Pause song
 function pauseSong() {
-    musicContainer.classList.remove('play');
-    playBtn.querySelector('i.fas').classList.add('fa-play')
-    playBtn.querySelector('i.fas').classList.remove('fa-pause')
-    audio.pause()
-
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+  audio.pause();
 }
-// previous song 
 
+// previous song
 function prevSong() {
-    songIndex--
-    if (songIndex < 0) {
-        songIndex = songs.length - 1
-    }
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
 
-    loadSong(songs[songIndex])
-    playSong()
+  loadSong(songs[songIndex]);
+  playSong();
 }
 
 // Next song
 function nextSong() {
-    songIndex++
-    if (songIndex > songs.length - 1) {
-        songIndex = 0
-    }
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
 
-    loadSong(songs[songIndex])
-    playSong()
-
+  loadSong(songs[songIndex]);
+  playSong();
 }
 
 // update progress bar
 function updateProgress(e) {
-    const { duration, currentTime } = e.srcElement
-    const progressPercent = (currentTime / duration) * 100
-    progress.style.width = `${progressPercent}%`
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
 }
 
 // set progress bar
 function setProgress(e) {
-    const width = this.clientWidth
-    const clickX = e.offsetX
-    const duration = audio.duration
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
 
-    audio.currentTime = (clickX / width) * duration
-
+  audio.currentTime = (clickX / width) * duration;
 }
 
-
-
 // Event Listeners
-playBtn.addEventListener('click', () => {
-    const isPlaying = musicContainer.classList.contains('play')
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
 
-    if (isPlaying) {
-        pauseSong()
-    } else {
-        playSong()
-    }
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
 });
 
 //change Song events
-prevBtn.addEventListener('click', prevSong)
-nextBtn.addEventListener('click', nextSong)
-
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
 
 // update time of song
-audio.addEventListener('timeupdate', updateProgress)
+audio.addEventListener("timeupdate", updateProgress);
 
 // Click on progress bar
-progressContainer.addEventListener('click', setProgress)
+progressContainer.addEventListener("click", setProgress);
 
-
-audio.addEventListener('ended', nextSong)
-
-
-
+// to play the next song
+audio.addEventListener("ended", nextSong);
